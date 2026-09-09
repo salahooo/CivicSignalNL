a0be590d6909d4407bcfa8b8d75165896092dcca
Salah Abdulkader <salah_fa3ore@hotmail.com>
2026-09-09 11:33:46 +0200

    feat: add scheduler monitoring dashboard

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 import { AmsterdamSourcePanel } from './AmsterdamSourcePanel'
+import { SchedulerPanel } from './SchedulerPanel'
 import './styles.css'
 
-createRoot(document.getElementById('root')!).render(<StrictMode><App /><AmsterdamSourcePanel viewOfficial={() => { window.location.assign('/?sourceType=OFFICIAL_OPEN_DATA&page=0&size=20') }} /></StrictMode>)
+createRoot(document.getElementById('root')!).render(<StrictMode><App /><AmsterdamSourcePanel viewOfficial={() => { window.location.assign('/?sourceType=OFFICIAL_OPEN_DATA&page=0&size=20') }} /><SchedulerPanel /></StrictMode>)
